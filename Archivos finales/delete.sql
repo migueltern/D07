9af81c5d274f5863cb92408aa16799c05555a3b3
Miguel Ternero <migueltern@gmail.com>
2018-01-19 19:43:48 +0100

    commi

--- Archivos finales/delete.sql
+++ Archivos finales/delete.sql
@@ -2,8 +2,8 @@ start transaction;
 
 use `Acme-Explorer`;
 
-revoke all privileges on `Acme-Pad-Thai`.* from 'acme-user'@'%';
-revoke all privileges on `Acme-Pad-Thai`.* from 'acme-manager'@'%';
+revoke all privileges on `Acme-Explorer`.* from 'acme-user'@'%';
+revoke all privileges on `Acme-Explorer`.* from 'acme-manager'@'%';
 
 drop user 'acme-user'@'%';
 drop user 'acme-manager'@'%';
